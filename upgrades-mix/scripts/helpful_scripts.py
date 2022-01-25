b1128ab4eef3df3f0f6eb88b9b10721cf6de5911
jwhan77 <gksjeongw96@gmail.com>
2022-01-26 00:11:27 +0900

    Upgrades mix

---
+++ upgrades-mix/scripts/helpful_scripts.py
@@ -0,0 +1,56 @@
+from brownie import network, accounts, config
+import eth_utils
+
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache", "mainnet-fork"]
+
+
+def get_account(number=None):
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
+    if number:
+        return accounts[number]
+    if network.show_active() in config["networks"]:
+        account = accounts.add(config["wallets"]["from_key"])
+        return account
+    return None
+
+
+# initializer=box.store, 1
+def encode_function_data(initializer=None, *args):
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
+
+
+def upgrade(
+    account,
+    proxy,
+    new_implementation_address,
+    proxy_admin_contract=None,
+    initializer=None,
+    *args
+):
+    transaction = None
+    if proxy_admin_contract:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy_admin_contract.upgradeAndCall(
+                proxy.address,
+                new_implementation_address,
+                encoded_function_call,
+                {"from": account},
+            )
+        else:
+            transaction = proxy_admin_contract.upgrade(
+                proxy.address, new_implementation_address, {"from": account}
+            )
+    else:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy.upgradeToAndCall(
+                new_implementation_address, encoded_function_call, {"from": account}
+            )
+        else:
+            transaction = proxy.upgradeTo(new_implementation_address, {"from": account})
+    return transaction
